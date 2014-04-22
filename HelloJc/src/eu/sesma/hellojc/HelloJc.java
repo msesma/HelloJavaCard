c532e52327e0da40b9425089ea67a5a51de0f3f0
Miguel Sesma <msesma@paradigmatecnologico.com>
2014-04-22 14:46:03 +0200

    Initial commit

---
+++ HelloJc/src/eu/sesma/hellojc/HelloJc.java
@@ -0,0 +1,121 @@
+
+package eu.sesma.hellojc;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.Util;
+
+public class HelloJc extends Applet {
+
+    private static final byte[] INPUT = {
+            'H', 'e', 'l', 'l', 'o',
+    };
+    private static final byte[] OUTPUT = {
+            'G', 'o', 'o', 'd', 'b', 'y', 'e'
+    };
+    private static final byte CLA = (byte) 0x00;
+    // CLA MASK allows for different channels
+    private static final byte CLA_MASK = (byte) 0xFC;
+    private static final byte HELLO_INS = (byte) 0xBB;
+
+    protected HelloJc() {
+    }
+
+    public static void install(final byte bArray[], final short bOffset, final byte bLength) throws ISOException {
+        new HelloJc().register();
+    }
+
+    // The JCRE dispatches incoming APDUs to the process method.The APDU object is owned and maintained by the JCRE. It
+    // encapsulates details of the underlying transmission protocol (T0 or T1 as specified in ISO 7816-3) by providing a
+    // common interface.
+    public void process(final APDU apdu) throws ISOException {
+        if (selectingApplet()) {
+            return;
+        }
+
+        byte[] buffer = apdu.getBuffer();
+        if ((buffer[ISO7816.OFFSET_CLA] & CLA_MASK) != CLA) {
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
+
+        byte ins = buffer[ISO7816.OFFSET_INS];
+        switch (ins) {
+            case HELLO_INS:
+                getHelloWorld(apdu);
+                break;
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
+    }
+
+    private void getHelloWorld(final APDU apdu) throws ISOException {
+        byte[] buffer = apdu.getBuffer();
+        // assume this command has incoming data Lc tells us the incoming apdu command length
+        short bytesLeft = (short) (buffer[ISO7816.OFFSET_LC] & 0x00FF);
+        if (bytesLeft < (short) INPUT.length) {
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        }
+        short readCount = apdu.setIncomingAndReceive();
+        while (bytesLeft > 0) {
+            // process bytes in buffer[5] to buffer[readCount+4];
+            bytesLeft -= readCount;
+            readCount = apdu.receiveBytes(ISO7816.OFFSET_CDATA);
+        }
+
+        if (contains(buffer, INPUT) == ISO7816.OFFSET_LC + 1) {
+            short length = (short) OUTPUT.length;
+            // inform system that the applet has finished processing the command and the system should now prepare to
+            // construct a response APDU which contains data field
+            short le = apdu.setOutgoing();
+            // Unfortunately setOutgoing always returns 256 for Le so lets retrieve it in other way
+            le = buffer[(short) (ISO7816.OFFSET_LC + buffer[ISO7816.OFFSET_LC] + 1)];
+            if (le != 0 && le < length) {
+                ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+            }
+
+            // informs the CAD the actual number of bytes returned
+            apdu.setOutgoingLength((byte) length);
+
+            // Set the response data
+            Util.arrayCopyNonAtomic(OUTPUT, (short) 0, buffer, (short) 0, length);
+
+            apdu.sendBytes((short) 0, length);
+        } else {
+            ISOException.throwIt(ISO7816.SW_WRONG_DATA);
+        }
+    }
+
+    public short contains(final byte[] data, final byte[] pattern) {
+        if (data == null || pattern == null || data.length < pattern.length) {
+            return -1;
+        }
+
+        byte j;
+        for (short i = 0; i < (short) (data.length - pattern.length + 1); i++) {
+            j = 0;
+            while (j < pattern.length && data[(short) (i + j)] == pattern[j]) {
+                j++;
+            }
+            if (j == pattern.length) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // UNUSED
+
+    // This method is called by the JCRE to indicate that this applet has been selected. It performs necessary
+    // initialization, which is required to process the subsequent APDU messages.
+    public boolean select() {
+        return true;
+    }
+
+    // This method is called by the JCRE to inform the applet that it should perform any clean-up and bookkeeping tasks
+    // before the applet is deselected.
+    public void deselect() {
+    }
+
+}
---
+++ HelloJctest/test/eu/sesma/hellojc/HelloJctest.java
@@ -0,0 +1,184 @@
+
+package eu.sesma.hellojc;
+
+import com.licel.jcardsim.io.CAD;
+import com.licel.jcardsim.io.JavaxSmartCardInterface;
+
+import junit.framework.Assert;
+
+import org.bouncycastle.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+import javacard.framework.AID;
+import javacard.framework.ISO7816;
+
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+
+@SuppressWarnings("deprecation")
+public class HelloJctest {
+
+    HelloJc applet;
+
+    private static final byte[] APPLET_AID = {
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x00
+    };
+    AID appletAID;
+    JavaxSmartCardInterface simulator;
+
+    @Before
+    public void setup() {
+        // For testing utility methods
+        applet = new HelloJc();
+
+        appletAID = new AID(APPLET_AID, (short) 0, (byte) APPLET_AID.length);
+
+        // 2 - Local Mode with ResponseAPDU transmitCommand(CommandAPDU) method
+        System.setProperty("com.licel.jcardsim.terminal.type", "2");
+        CAD cad = new CAD(System.getProperties());
+
+        simulator = (JavaxSmartCardInterface) cad.getCardInterface();
+        simulator.installApplet(appletAID, HelloJc.class);
+
+    }
+
+    @Test
+    public void testSelectAppletShouldReturnOk() {
+        Assert.assertTrue(simulator.selectApplet(appletAID));
+    }
+
+    @Test
+    public void testProcessIncorrectClaShouldReturnSW_CLA_NOT_SUPPORTED() {
+        simulator.selectApplet(appletAID);
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x80, 0xbc, 0x00, 0x00));
+        Assert.assertEquals(ISO7816.SW_CLA_NOT_SUPPORTED, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessCorrectClaNonZeroChannelShouldReturnSW_INS_NOT_SUPPORTED() {
+        simulator.selectApplet(appletAID);
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x01, 0xbc, 0x00, 0x00));
+        Assert.assertEquals(ISO7816.SW_INS_NOT_SUPPORTED, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessUnsupportedApduShouldReturnSW_INS_NOT_SUPPORTED() {
+        simulator.selectApplet(appletAID);
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbc, 0x00, 0x00));
+        Assert.assertEquals(ISO7816.SW_INS_NOT_SUPPORTED, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessHelloApduCase2ShouldReturnSW_WRONG_DATA() {
+        simulator.selectApplet(appletAID);
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbb, 0x00, 0x00, 0x7));
+        Assert.assertEquals(ISO7816.SW_WRONG_DATA, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessHelloApduWrongDataLenghtShouldReturnSW_WRONG_LENGTH() {
+        simulator.selectApplet(appletAID);
+        byte[] data = {
+                0x01, 0x02, 0x03, 0x04
+        };
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbb, 0x00, 0x00, data));
+        Assert.assertEquals(ISO7816.SW_WRONG_LENGTH, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessHelloApduWrongDataShouldReturnSW_WRONG_DATA() {
+        simulator.selectApplet(appletAID);
+        byte[] data = {
+                'H', 'e', 'l', 'l', 'a',
+        };
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbb, 0x00, 0x00, data));
+        Assert.assertEquals(ISO7816.SW_WRONG_DATA, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessHelloApduGoodDataWrongLeShouldReturnSW_WRONG_LENGTH() {
+        simulator.selectApplet(appletAID);
+        byte[] data = {
+                'H', 'e', 'l', 'l', 'o',
+        };
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbb, 0x00, 0x00, data, 0x06));
+        Assert.assertEquals(ISO7816.SW_WRONG_LENGTH, (short) response.getSW());
+    }
+
+    @Test
+    public void testProcessHelloApduGoodDataShouldReturnGoodbyeAndOK() {
+        simulator.selectApplet(appletAID);
+        byte[] data = {
+                'H', 'e', 'l', 'l', 'o',
+        };
+        byte[] responseData = {
+                'G', 'o', 'o', 'd', 'b', 'y', 'e'
+        };
+
+        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(0x00, 0xbb, 0x00, 0x00, data));
+        Assert.assertTrue(Arrays.areEqual(responseData, response.getData()));
+        Assert.assertEquals(ISO7816.SW_NO_ERROR, (short) response.getSW());
+    }
+
+    @Test
+    public void testContainsNullDataShouldReturnMinusOne() {
+        byte[] nullArray = null;
+        Assert.assertEquals(-1, applet.contains(nullArray, new byte[] {
+                0, 0
+        }));
+    }
+
+    @Test
+    public void testContainsNullPatternShouldReturnMinusOne() {
+        byte[] nullArray = null;
+        Assert.assertEquals(-1, applet.contains(new byte[] {
+                0, 0
+        }, nullArray));
+    }
+
+    @Test
+    public void testContainsShortDataPatternShouldReturnMinusOne() {
+        Assert.assertEquals(-1, applet.contains(new byte[] {
+                0, 0
+        }, new byte[] {
+                0, 0, 0
+        }));
+    }
+
+    @Test
+    public void testContainsNoPatternInDataShouldReturnMinusOne() {
+        Assert.assertEquals(-1, applet.contains(new byte[] {
+                0, 1, 2, 3, 4, 5
+        }, new byte[] {
+                1, 2, 4
+        }));
+    }
+
+    @Test
+    public void testContainsShouldReturnTwo() {
+        Assert.assertEquals(2, applet.contains(new byte[] {
+                0, 1, 2, 3, 4, 5
+        }, new byte[] {
+                2, 3, 4
+        }));
+    }
+
+    public int contains(final byte[] data, final byte[] pattern) {
+        if (data == null || pattern == null || data.length < pattern.length) {
+            return -1;
+        }
+
+        int j;
+        for (int i = 0; i < data.length - pattern.length + 1; i++) {
+            j = 0;
+            while (j < pattern.length && data[i + j] == pattern[j]) {
+                j++;
+            }
+            if (j == pattern.length) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
